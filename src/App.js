47a6915610f75dfcc6cf57235f0580ca9c1af2b5
Igor Jelen <igorjelen@gmail.com>
2019-08-20 11:30:36 +0100

    Add Header component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import Homepage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shoppage/shoppage.component";
+import Header from "./components/header/header.component";
 import { Route, Switch } from "react-router-dom";
 
 const page404 = () => <p>Page 404</p>;
@@ -9,6 +10,7 @@ const page404 = () => <p>Page 404</p>;
 function App() {
   return (
     <>
+      <Header />
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route path="/shop" component={ShopPage} />
